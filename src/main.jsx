cd4608f0a95a77e3f2c7155acfeddf34c65bce97
Syed Mohammad Yasir <smy329@gmail.com>
2023-05-02 03:05:18 +0600

    firebase configured

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App.jsx';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './routes/index.jsx';
+import { Toaster } from 'react-hot-toast';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
