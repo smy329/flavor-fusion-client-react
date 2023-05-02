fecbb7e9eeb210c519454626e18618ebd084c38b
Syed Mohammad Yasir <smy329@gmail.com>
2023-05-03 02:08:34 +0600

    banner ready

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import bannerImg from '../../assets/bg/loginPage.jpeg';
+import bannerImg from '../../assets/bannerImg.png';
 
 const Banner = () => {
   return (
-    <div className="grid grid-cols-2 gap-5">
-      <div>
-        <h1 className="text-6xl font-black uppercase leading-tight text-amber-500">
+    <div className="grid grid-cols-2 gap-5 items-center">
+      <div className="w-11/12">
+        <h1 className="text-6xl font-black uppercase leading-tight text-amber-500 mb-4 drop-shadow-xl">
           Bringing flavor to your table
         </h1>
-        <p>
+        <p className="font-semibold text-lg text-gray-500 w-10/12">
           Spice up your kitchen with our recipe collection - where every dish is
           a masterpiece waiting to be created. From classic comfort food to
           exotic cuisine, discover your new go-to meal here
         </p>
       </div>
       <div>
-        <img src={bannerImg} alt="" />
+        <img src={bannerImg} alt="" className="drop-shadow-2xl" />
       </div>
     </div>
   );
